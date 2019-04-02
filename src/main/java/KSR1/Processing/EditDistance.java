8cd26e4aa5237d915f093ebb4a0b149f229ff91c
Krzysztof-Wierzbicki <krzysztof.wierzbicki.wk@gmail.com>
2019-04-02 17:29:26 +0200

    Added similarity calculation

---
+++ src/main/java/KSR1/Processing/EditDistance.java
@@ -0,0 +1,9 @@
+package KSR1.Processing;
+
+public class EditDistance implements Similarity {
+    @Override
+    public double compare(String a, String b) {
+        throw new RuntimeException("Not implemented");
+        //return 0;
+    }
+}
